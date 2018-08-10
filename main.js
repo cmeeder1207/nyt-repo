e96e30095b1f5c610949c441e228438f33b611a7
Jack <jhkent88@gmail.com>
2018-08-10 14:18:13 -0700

    Connect fields and submit to API call

--- main.js
+++ main.js
@@ -2,23 +2,20 @@
 // --------------------
 
 
-
 // FUNCIONS
 // --------------------
 
 function getArticles(search, begin, end, num) {
-    var searchTerm = search;
-    var numRecords = num;
-    var begin_date = begin;
-    var end_date = end;
+
+    console.log(search, begin, end, num);
 
 
     var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
     queryURL += '?' + $.param({
     'api-key': "b9f91d369ff59547cd47b931d8cbc56b:0:74623931",
-    'q': searchTerm,
-    'begin_date': begin_date,
-    'end_date': end_date,
+    'q': search,
+    'begin_date': begin,
+    'end_date': end,
     });
 
     $.ajax({
@@ -26,20 +23,29 @@ function getArticles(search, begin, end, num) {
       method: "GET"
     }).then(function(r) {
 
-      for (var i = 0; i < numRecords; i++) {
+      for (var i = 0; i < num; i++) {
       console.log(r.response.docs[i].headline.main);
       // console.log(r.response.docs[i].pub_date);
       // console.log(r.response.docs[i].snippet);
       // console.log(r.response.docs[i].web_url);
       };
 
-
     });
+};
 
 // CALLS
 // --------------------
 // $('submit').on click...
     // call getArticles()
+
+$(':submit').on('click', function(event) {
+    event.preventDefault();
+    var search = $('#searchTerm').val();
+    var begin = $('#startYear').val();
+    var end = $('#endYear').val();
+    var num = $('#numberOfRecords').val();
+    getArticles(search, begin, end, num);
+})
 
 
 
